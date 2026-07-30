9ff623c22f381bf8df069ad40b0f110faf32f770
BCharash <Bill.charash@gmail.com>
2026-07-30 13:37:10 +0100

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -38,24 +38,6 @@ async function initializeApplication() {
 }
 
 
-//--------------------------------------------------
-// Beach Display
-//--------------------------------------------------
-
-function displayBeach(beachId) {
-
-    const beach = findBeach(beachId);
-
-    if (!beach)
-        return;
-
-    regionSpan.textContent = beach.region;
-    municipalitySpan.textContent = beach.municipality;
-    districtSpan.textContent = beach.district;
-    latitudeSpan.textContent = beach.latitude ?? "";
-    longitudeSpan.textContent = beach.longitude ?? "";
-
-}
 
 
 //--------------------------------------------------
