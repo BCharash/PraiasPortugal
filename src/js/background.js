5c02fd5cea4999514e6a30a0222b35a1bc2aecf0
BCharash <Bill.charash@gmail.com>
2026-07-31 13:00:22 +0100

    Create background.js

---
+++ src/js/background.js
@@ -0,0 +1,9 @@
+//
+// background.js
+//
+// Controls the visual background of the dashboard.
+//
+
+export function updateBackground(condition) {
+    console.log(`Background changed to: ${condition}`);
+}
