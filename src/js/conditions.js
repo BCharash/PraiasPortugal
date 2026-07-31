3a1abee29aad7a4e87467d0c272629c2fc11e536
BCharash <Bill.charash@gmail.com>
2026-07-31 14:55:45 +0100

    Create conditions.js

---
+++ src/js/conditions.js
@@ -0,0 +1,17 @@
+//--------------------------------------------------
+// Current Conditions Widget
+//--------------------------------------------------
+
+function initializeConditions() {
+
+    // Placeholder for future initialization.
+
+}
+
+
+function updateConditions(beach) {
+
+    // Placeholder until live weather and ocean data
+    // are connected.
+
+}
