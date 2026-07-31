c1f747483a3c8a726590729cb746d4a478fefe94
BCharash <Bill.charash@gmail.com>
2026-07-31 16:51:13 +0100

    Update weatherFormatter.js
    uv index

--- src/js/weatherFormatter.js
+++ src/js/weatherFormatter.js
@@ -47,6 +47,33 @@ function formatWindGusts(weather) {
 }
 
 
+/*
+ * Format the UV Index.
+ */
+function formatUV(weather) {
+
+    if (weather.uvIndex == null)
+        return "--";
+
+    const uv = Math.round(weather.uvIndex);
+
+    if (uv <= 2)
+        return `${uv} Low`;
+
+    if (uv <= 5)
+        return `${uv} Moderate`;
+
+    if (uv <= 7)
+        return `${uv} High`;
+
+    if (uv <= 10)
+        return `${uv} Very High`;
+
+    return `${uv} Extreme`;
+
+}
+
+
 //--------------------------------------------------
 // Private Functions
 //--------------------------------------------------
