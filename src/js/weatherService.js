48d5ad1d129a72da9bcc94d41ec8a757aa68ee2b
BCharash <Bill.charash@gmail.com>
2026-07-31 15:13:36 +0100

    Create weatherService.js

---
+++ src/js/weatherService.js
@@ -0,0 +1,29 @@
+//--------------------------------------------------
+// Weather Service
+//
+// Retrieves current weather conditions for a beach.
+//--------------------------------------------------
+
+
+//--------------------------------------------------
+// Public Functions
+//--------------------------------------------------
+
+async function getCurrentWeather(beach) {
+
+    // Placeholder until we connect Open-Meteo.
+
+    return {
+
+        airTemperature: "--",
+        windSpeed: "--",
+        windDirection: "--"
+
+    };
+
+}
+
+
+//--------------------------------------------------
+// Private Functions
+//--------------------------------------------------
