85ba12442bc3c33fc273c919e1ecf8e853b6997b
BCharash <Bill.charash@gmail.com>
2026-07-31 17:48:00 +0100

    Update conditions.js

--- src/js/conditions.js
+++ src/js/conditions.js
@@ -62,10 +62,8 @@ function updateConditions(dashboardData) {
     //--------------------------------------------------
 
     if (weather) {
-        
-//debug console.log
 
-        console.log(weather);
+
 
         airTempElement.textContent      = formatAirTemperature(weather);
         airFeelsLikeElement.textContent = formatFeelsLike(weather);
