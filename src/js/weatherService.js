bbdc07bb58b5f3b4f31e55efb2de50c72a429b5d
BCharash <Bill.charash@gmail.com>
2026-07-31 15:14:14 +0100

    Update weatherService.js

--- src/js/weatherService.js
+++ src/js/weatherService.js
@@ -11,13 +11,23 @@
 
 async function getCurrentWeather(beach) {
 
-    // Placeholder until we connect Open-Meteo.
+    const url =
+        `https://api.open-meteo.com/v1/forecast` +
+        `?latitude=${beach.latitude}` +
+        `&longitude=${beach.longitude}` +
+        `&current=temperature_2m,wind_speed_10m,wind_direction_10m`;
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    console.log("Weather Service:", data);
 
     return {
 
-        airTemperature: "--",
-        windSpeed: "--",
-        windDirection: "--"
+        airTemperature: data.current.temperature_2m,
+        windSpeed: data.current.wind_speed_10m,
+        windDirection: data.current.wind_direction_10m
 
     };
 
