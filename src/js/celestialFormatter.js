2d5ed917a6707c4544dbf3cc3becf705cd4319b7
BCharash <Bill.charash@gmail.com>
2026-08-19 14:47:28 +0100

    celestial Formatter created

---
+++ src/js/celestialFormatter.js
@@ -0,0 +1,22 @@
+//--------------------------------------------------
+// Celestial Formatter
+//
+// Creates the visual representation of the sky.
+//--------------------------------------------------
+
+
+//--------------------------------------------------
+// Public Functions
+//--------------------------------------------------
+
+function renderCelestialGraphic(
+    celestialState,
+    weather
+) {
+
+    if (!celestialState)
+        return "";
+
+    return "";
+
+}
